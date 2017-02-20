1952d362371521fc6989207c7bb33497f9a9deb2
sanquan <sanquan@juanpi.com>
2017-02-20 09:50:37 +0800

    remove config api address

--- core/config.js
+++ core/config.js
@@ -3,38 +3,38 @@
 module.exports = {
     development: { // 开发环境
         ServiceConf: {
-            serviceServer: '192.168.160.121:9000',
-            serviceApiServer: '192.168.160.121:8000'
+            serviceServer: '127.0.0.1:9000',
+            serviceApiServer: '127.0.0.1:8000'
         },
         ConfigConf: {
-            configServer: 'configApi.juanpi.org:8080'
+            configServer: '127.0.0.1:8080'
         },
         MonitorConf: {
-            monitorServer: 'monitor.juanpi.org',
+            monitorServer: '127.0.0.1',
         }
     },
     test: { // 测试环境
         ServiceConf: {
-            serviceServer: '192.168.160.121:9000',
-            serviceApiServer: '192.168.160.121:8000'
+            serviceServer: '127.0.0.1:9000',
+            serviceApiServer: '127.0.0.1:8000'
         },
         ConfigConf: {
-            configServer: 'configApi.juanpi.org:8080' // 测试
+            configServer: '127.0.0.1:8080' // 测试
         },
         MonitorConf: {
-            monitorServer: 'monitor.juanpi.org'
+            monitorServer: '127.0.0.1'
         }
     },
     production: { // 生产环境
         ServiceConf: {
-            serviceServer: 'api.service.juanpi.org',
-            serviceApiServer: 'api.devtesttool.juanpi.org'
+            serviceServer: '127.0.0.1',
+            serviceApiServer: '127.0.0.1'
         },
         ConfigConf: {
-            configServer: 'api.config.juanpi.org:80' // 生产
+            configServer: '127.0.0.1' // 生产
         },
         MonitorConf: {
-            monitorServer: 'monitorapi.juanpi.com'
+            monitorServer: '127.0.0.1'
         }
     }
 }
